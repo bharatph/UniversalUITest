d6039457dabe4f1ce6b09f203546441a08bea45e
Bharatvaj <bharatvajph@gmail.com>
2018-03-21 20:35:29 +0530

    Vision level #6 - Normalizing with other repos

--- app/test.cpp
+++ app/test.cpp
@@ -1,36 +1,44 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include <UniversalUI.hpp>
-#ifdef FLTK_UI
-#include<FLTKUI.hpp>
-#elif CHARACTER_UI
-#include<CharacterUI.hpp>
-#endif
+#include <iostream>
+#include <vector>
+#include <map>
+#include <clog/clog.h>
 #include <Node/Node.hpp>
+#include <UniversalUI.hpp>
+#if defined(FLTK_UI)
+#include <FLTKUI.hpp>
+#elif defined(CHARACTER_UI)
+#include <CharacterUI.hpp>
+#endif
 
 using namespace std;
 using namespace uui;
 
-
-typedef struct {
+typedef struct
+{
 	std::string hostname;
 	int port;
 } host_data;
+
+int lol_test(uui::UI *ui, void *obj)
+{
+	std::string lol = "something is lolly";
+	ui->error(lol);
+	ui->alert("hello");
+}
 
 int test_connection(uui::UI *ui, void *obj)
 {
 	std::string hostname = ((host_data *)obj)->hostname;
 	int port = ((host_data *)obj)->port;
 	node::Node *n = new node::Node();
-	if(n->connect(hostname.c_str(), port) != 0)
+	if (n->connect(hostname.c_str(), port) != 0)
 	{
 		ui->error("Cannot connect to the service");
 		return 0;
 	}
 	log_inf("TEST", "All are working fine");
 	n->writeln("OkokOkokayyOkay...");
-	ui->alert( n->readln() );
+	ui->alert(n->readln());
 	return 0;
 }
 
@@ -39,16 +47,19 @@ void *make_args(std::string s, int i)
 	return nullptr;
 }
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 #ifdef FLTK_UI
-	//UI *ui = new FLTKUI();
+	UI *ui = new FLTKUI();
 #else
-	//UI *ui = new CharacterUI();
+	UI *ui = CharacterUI::getInstance();
 #endif
-	FLTKUI *ui = new FLTKUI();
 	ui->error("Why?");
+	ui->alert("I am going to walk you through, are you ready?");
+	ui->alert("Be sure to install the dependencies before you continue");
 	ui->set("connect", new vector<std::string>{"Hostname", "Port"}, test_connection);
-	ui->run("connect", new host_data {"localhost", 22});
+	ui->set("lol", new vector<std::string>{"LOL", "another lOl"}, lol_test);
+	ui->run("connect", new host_data{"localhost", 22});
+	ui->run("lol", nullptr);
 	return 0;
 }
